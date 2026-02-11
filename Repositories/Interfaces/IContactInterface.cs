da58924cf1c91efff6307f97af296ac1641d9a91
Deshmukh Himani <deshmukhhimani1@gmail.com>
2026-02-11 14:09:15 +0530

    Initial commit: Ketan MVC project

---
+++ Repositories/Interfaces/IContactInterface.cs
@@ -0,0 +1,11 @@
+namespace Repositories;
+
+public interface IContactInterface
+{
+    Task<List<t_Contact>> GetAll();
+    Task<List<t_Contact>> GetAllByUser(string userid);
+    Task<t_Contact> GetOne(string contactid);
+    Task<int> Add(t_Contact contactData);
+    Task<int> Update(t_Contact contactData);
+    Task<int> Delete(string contactid);
+}
---
+++ Repositories/Interfaces/IUserInterface.cs
@@ -0,0 +1,8 @@
+﻿namespace Repositories;
+
+public interface IUserInterface
+{
+    Task<int> Register(t_User user);
+    Task<t_User> Login(vm_Login user);
+    Task<t_User> GetUser(int userid);
+}
